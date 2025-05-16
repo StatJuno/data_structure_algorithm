b5e9911df0575219050808c2c6c90542a7bb3f2c
Joonho Kim <joonhokjh@gmail.com>
2025-05-17 02:42:11 +0900

    [Bronze III] Title: ACM 호텔, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for i in range(T):
+    H, W, N = map(int, input().split())
+    
+    floor = N % H
+    num = N // H +1
+    
+    if floor == 0 :
+        floor = H
+        num -= 1
+    
+    print(floor*100 + num)
